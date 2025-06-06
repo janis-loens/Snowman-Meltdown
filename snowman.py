fb3d59f8b3408f2de15b5876eb3636dd5eabd96e
Janis Löns <janis.loens@outlook.de>
2025-06-06 15:17:00 +0200

    Deleted some comments.

--- snowman.py
+++ snowman.py
@@ -1,9 +1,6 @@
 import game_logic
 
 WORDS = ["python", "git", "github", "snowman", "meltdown"]
-# ―――――――――――――――――――――――――――――
-# Main game
-# ―――――――――――――――――――――――――――――
 
 if __name__ == "__main__":
     game_logic.play_game(WORDS)
